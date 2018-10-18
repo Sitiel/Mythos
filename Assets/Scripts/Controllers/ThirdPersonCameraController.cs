62fb6ce0fd5f8118f8377c299e1e1d031b5888cf
valerian <valerian@kang.fr>
2018-10-19 00:17:18 +0300

    third person controller / third person camera
    Add some really simple third person camera controller and player controller

---
+++ Assets/Scripts/Controllers/ThirdPersonCameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour {
+
+    [Header("Player to follow")]
+    [SerializeField]
+    Transform lookAt;
+
+    [SerializeField]
+    private float distance = 10.0f;
+
+    [SerializeField]
+    private float minYAngle;
+    [SerializeField]
+    private float maxYAngle;
+    [Header("Layer(s) to include for camera collision")]
+    public LayerMask CamOcclusion;
+
+    private float currentX = 0, currentY = 0;
+    private float sensivityX = 4.0f, sensivityY = 4.0f;
+
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        currentX += Input.GetAxis("Mouse X")*sensivityX;
+        currentY = Mathf.Clamp(currentY - Input.GetAxis("Mouse Y") * sensivityY, minYAngle, maxYAngle);
+        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel"), 5, 25);
+    }
+
+
+    private void collisionReplacementForCamera(){
+        RaycastHit wallHit = new RaycastHit();
+
+        //We trace a ray between the camera and the player and we put the camera at the collision point if there is something
+        if (Physics.Linecast(lookAt.position, transform.position, out wallHit, CamOcclusion))
+        {
+            transform.position = new Vector3(wallHit.point.x + wallHit.normal.x * 0.5f, wallHit.point.y + wallHit.normal.y * 0.5f, wallHit.point.z + wallHit.normal.z * 0.5f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * dir;
+        collisionReplacementForCamera();
+
+        transform.LookAt(new Vector3(lookAt.position.x, (lookAt.position.y + 2f), lookAt.position.z));
+
+    }
+}
---
+++ Assets/Scripts/Controllers/ThirdPersonPlayerController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ThirdPersonPlayerController : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    [SerializeField]
+    private ThirdPersonCameraController cameraController;
+
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private float speed = 5;
+
+    private float mass = 3.0f;
+    float impact = 0;
+
+    private float gravity = 3f;
+    bool jumping = false;
+    float vSpeed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void jump(float force)
+    {
+        if (!jumping)
+        {
+            impact += force / mass;
+            jumping = true;
+            //playerAnimator.SetInteger("Jumping", 1);
+        }
+    }
+
+    private void calculateVSpeed(){
+        
+        vSpeed += gravity * Time.deltaTime;
+
+        //We do not reduce the impact force using vSpeed to create a Quadradic like jump
+
+        if (impact > 0.2) vSpeed = -impact * Time.deltaTime;
+
+        //Lerp is used in the jump to decrease the impact slowly to 0
+
+        impact = Mathf.Lerp(impact, 0, 5 * Time.deltaTime);
+
+        //If we touch the ground, we are not falling anymore so our vSpeed is 0 (if vSpeed is negative we are jumping)
+        if (characterController.isGrounded && vSpeed >= 0)
+        {
+            vSpeed = 0;
+        }
+    }
+
+
+    private void calculateCurrentState(){
+        if (characterController.isGrounded && jumping && impact < 0.2)
+        {
+            jumping = false;
+            //playerAnimator.SetInteger("Jumping", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        int reversed = 1, reverseAngle = 0;
+
+        float direction = 0;
+
+        if (Input.GetKey("z"))
+        {
+            transform.rotation = cameraController.transform.rotation;
+            direction = speed;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            reverseAngle = 180;
+            reversed = -1;
+            direction = speed;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, cameraController.transform.rotation.eulerAngles.y + 180, 0f));
+        }
+
+        /*if (direction > 0)
+        {
+            playerAnimator.SetFloat("Speed", 2);
+        }
+        else
+        {
+            playerAnimator.SetFloat("Speed", 0);
+        }*/
+
+        transform.rotation = Quaternion.Euler(new Vector3(0f, transform.rotation.eulerAngles.y, 0f));
+
+        if (Input.GetKey("q"))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, cameraController.transform.rotation.eulerAngles.y + reverseAngle - (45 * reversed), 0f));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, cameraController.transform.rotation.eulerAngles.y + reverseAngle + (45 * reversed), 0f));
+        }
+
+        if (Input.GetKey("space"))
+        {
+            jump(100);
+        }
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * direction * Time.deltaTime;
+
+        forward.y = -vSpeed;
+        characterController.Move(forward);
+    }
+}
