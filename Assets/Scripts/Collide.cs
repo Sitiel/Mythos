0461d633070d65c17dbf1ab1db39b47c1aff8cb9
masho1 <masho@tlu.ee>
2018-11-27 02:22:41 +0200

    Rain
    Rain effect with sound and a wind

---
+++ Assets/Scripts/Collide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour {
+
+    public AudioSource rainSource;
+
+    private void Start()
+    {
+        rainSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "trigger")
+        {
+            rainSource.Play();
+            Destroy(col.gameObject);
+        }
+    }
+}
