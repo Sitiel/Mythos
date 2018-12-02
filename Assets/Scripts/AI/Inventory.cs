20dc693db8b91a4a6ba7a13f4f710690332eaf4e
DanielBessaix <danielbessaix@gmail.com>
2018-12-02 16:07:32 +0200

    Deactivate MultiJump, add attacks in air

---
+++ Assets/Scripts/AI/Inventory.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    //liste de chiffre d'arme
+    // 0 unarmed
+    // 1 2Handed sword
+    // 2 2Handed spear
+    // 3 2Handed axe
+    // 4 shield spear
+    // 
+    // 
+    // 
+    // 
+    // 
+
+    private List<int> weapons = new List<int>(0);
+    private int currentWeapon = 0;
+
+    
+    private bool twoHandedSword = true;
+    private bool twoHandedSpear = true;
+    private bool twoHandedAxe = true;
+    private bool shieldAndSpear = true;
+
+    public void InitialiseInventory()
+    {
+        weapons.Add(0);
+        if (twoHandedSword)
+        {
+            print("twhs");
+            weapons.Add(1);
+        }
+        if (twoHandedSpear)
+        {
+            weapons.Add(2);
+        }
+        if (twoHandedAxe)
+        {
+            weapons.Add(3);
+        }
+        if (shieldAndSpear)
+        {
+            weapons.Add(4);
+        }
+    }
+
+    public Inventory()
+    {
+        this.InitialiseInventory();
+    }
+
+    public void AddWeapon(int weaponNB)
+    {
+        if (!weapons.Contains(weaponNB))
+        {
+            weapons.Add(weaponNB);
+        }
+    }
+
+    public void RemoveWeapon(int weaponNB)
+    {
+        if (weapons.Contains(weaponNB))
+        {
+            weapons.Remove(weaponNB);
+        }
+    }
+
+    public void SelectWeapon(bool up)
+    {
+        int indexLastWeapon = weapons.IndexOf(currentWeapon);
+        
+        if (up){
+            if (indexLastWeapon == weapons.Count-1){
+                indexLastWeapon = -1;
+            }
+            currentWeapon = weapons[indexLastWeapon+1];
+        }
+        else{
+            
+            if (indexLastWeapon == 0){
+                indexLastWeapon = weapons.Count;
+            }
+            currentWeapon = weapons[indexLastWeapon-1];
+        }
+        print(indexLastWeapon);
+    }
+
+    public int GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+}
