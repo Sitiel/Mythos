7e439cd3deba207705c39e3c59306dceb0a5be61
masho1 <masho@tlu.ee>
2018-12-03 15:43:46 +0200

    Refined moon
    Refined code, added light and particle system

--- Assets/Scripts/Oscallator.cs
+++ Assets/Scripts/Oscallator.cs
@@ -8,22 +8,25 @@ public class Oscallator : MonoBehaviour {
     float width;
     float speed;
     float height;
+    float rotation = 0;
 
     void Start()
     {
-        speed = 1;
-        width = 200;
+        speed = 3f;
+        width = 400;
         height = 200;
     }
 
     void Update()
     {
-        timeCounter += Time.deltaTime*speed;
+        /*timeCounter += Time.deltaTime*speed;
 
         float x = Mathf.Cos(timeCounter)*width;
         float y = Mathf.Sin(timeCounter)*height;
         float z = Mathf.Cos(timeCounter)*width;
 
-        transform.position = new Vector3(x, y, z);
+        transform.position = new Vector3(x, y, z); */
+        transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+        rotation += Time.deltaTime * speed;
     }
 }
