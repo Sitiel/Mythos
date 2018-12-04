14a17f25bde47ab83d6769f02183248da19de8ed
Luis Arreaga <luism@tlu.ee>
2018-12-04 21:32:51 +0200

    PauseFunctionality

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        FindObjectOfType<ThirdPersonCameraController>().movingCamera = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        FindObjectOfType<ThirdPersonCameraController>().movingCamera = false;
+        Cursor.lockState = CursorLockMode.None; 
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -9,7 +9,7 @@ public class SettingsMenu : MonoBehaviour {
 
     public void SetVolume (float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("volume", volume );
     }
 
 }
